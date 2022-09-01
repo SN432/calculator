d9474f88c1c14a1e62aff8a9247872426d99c54f
Your Name <you@example.com>
2022-09-01 09:12:41 +0700

    added

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 
-function sum(a,b){
-    sum = a+b;
-    return console.log(sum) ;
-}
+// function sum(a,b){
+//     sum = a+b;
+//     return console.log(sum) ;
+// }
 
-function sub(a,b){
-    return console.log(a-b);
+//const { getJSDocEnumTag } = require("typescript");
+
+// function sub(a,b){
+//     return console.log(a-b);
+// }
+// function div(a,b){
+//     if(a === 0 ){
+//         return console.log("can not be divided by zero");
+//     }
+//     else{
+//         return console.log(a/b);
+//     }
+// }
+// function mul(a,b){
+//     return console.log(a*b);
+// }
+
+// sum(3,5)
+// sub(1,5)
+// mul(3,3)
+// div(10,2)
+// div(0,3)
+
+let outPutScreen = document.getElementById('output');
+
+function display(num){
+    document.getElementById('output').value += num;
 }
-function div(a,b){
-    if(a === 0 ){
-        return console.log("can not be divided by zero");
+function calculate(){
+    try{
+        document.getElementById('output').value = eval(document.getElementById('output').value);
+    }catch(err){
+        alert(error)
     }
-    else{
-        return console.log(a/b);
-    }
 }
-function mul(a,b){
-    return console.log(a*b);
-}
-
-sum(3,5)
-sub(1,5)
-mul(3,3)
-div(10,2)
-div(0,3)
-
-
